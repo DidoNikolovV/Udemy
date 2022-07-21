a4f95807700cfab090d72ae721f8e107a65b354d
Diyan Nikolov <dido_nikolov01@abv.bg>
2022-07-21 17:07:37 +0300

    Add spinner

--- React/seasons/src/SeasonDisplay.js
+++ React/seasons/src/SeasonDisplay.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import './SeasonDisplay.css';
+
+const seasonConfig = {
+  summer: {
+    text: 'Lets hit the beach!',
+    iconName: 'sun',
+  },
+  winter: {
+    text: 'Burr it is cold!',
+    iconName: 'snowFlake',
+  },
+};
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
@@ -10,8 +22,15 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = ({ lat }) => {
   const season = getSeason(lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
 
-  return <div>Season Display: {season}</div>;
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`}></i>
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`}></i>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
