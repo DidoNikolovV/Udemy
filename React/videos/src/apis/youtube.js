1252cdfce1ae292e52188c157ceb1c0fc0b04eb9
Diyan Nikolov <dido_nikolov01@abv.bg>
2022-07-23 09:44:45 +0300

    Fetch list of videos

---
+++ React/videos/src/apis/youtube.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyDZCA4INNO9FAAhxOHIrOXUMWvHVOoSo78';
+
+export default axios.create({
+  baseUrl: 'ttps://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    type: 'video',
+    maxResults: 5,
+    key: KEY,
+  },
+});
