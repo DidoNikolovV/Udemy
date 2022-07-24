b8f689bf91e6c1133b63a14d6f1f86cf4685589a
Diyan Nikolov <dido_nikolov01@abv.bg>
2022-07-24 15:10:09 +0300

    Add link to wikipedia page

--- React/widgets/src/components/Search.js
+++ React/widgets/src/components/Search.js
@@ -5,8 +5,6 @@ const Search = () => {
   const [term, setTerm] = useState('programming');
   const [results, setResults] = useState([]);
 
-  console.log(results);
-
   useEffect(() => {
     const search = async () => {
       const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
@@ -25,6 +23,25 @@ const Search = () => {
     search();
   }, [term]);
 
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            GO
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <div className="ui form">
@@ -37,6 +54,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
