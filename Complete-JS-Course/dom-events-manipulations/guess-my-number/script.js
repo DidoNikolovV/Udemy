c92151a7920e199ef5ac587a3851ed6036be70fb
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-08-27 12:30:33 +0300

    Implement game logic

--- Complete-JS-Course/dom-events-manipulations/guess-my-number/script.js
+++ Complete-JS-Course/dom-events-manipulations/guess-my-number/script.js
@@ -10,9 +10,38 @@
 
 // let guess = document.querySelector('.guess').value = 23;
 
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = document.querySelector('.score').textContent;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', () => {
     const guess = Number(document.querySelector('.guess').value);
+
     if (!guess) {
-        document.querySelector('.message').textContent = '⛔No number!'
+        document.querySelector('.message').textContent = '⛔No number!';
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉Correct number';
+    } else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Too high';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = '😢You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
+
+    } else if (guess < secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Too low';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = '😢You lost the game!';
+            document.querySelector('.score').textContent = 0;
+
+        }
+
     }
 })
