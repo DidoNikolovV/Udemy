2e4532fad680709d62f73e54144cc34c2f5bbef9
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-08-27 11:45:30 +0300

    Manipulate elements

---
+++ Complete-JS-Course/dom-events-manipulations/guess-my-number/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉Correct number';
+
+// let guess = document.querySelector('.score').value;
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+let guess = document.querySelector('.guess').value = 23;
