2b20729f49ab394f1fcc7e99e10dbe7ecbde06ba
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-08-27 12:11:19 +0300

    Handling events

--- Complete-JS-Course/dom-events-manipulations/guess-my-number/script.js
+++ Complete-JS-Course/dom-events-manipulations/guess-my-number/script.js
@@ -1,11 +1,18 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = '🎉Correct number';
+// document.querySelector('.message').textContent = '🎉Correct number';
 
-// let guess = document.querySelector('.score').value;
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// // let guess = document.querySelector('.score').value;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-let guess = document.querySelector('.guess').value = 23;
+// let guess = document.querySelector('.guess').value = 23;
+
+document.querySelector('.check').addEventListener('click', () => {
+    const guess = Number(document.querySelector('.guess').value);
+    if (!guess) {
+        document.querySelector('.message').textContent = '⛔No number!'
+    }
+})
